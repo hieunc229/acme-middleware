e60a834e5fb7ab0b5321fa099674306669ab72be
Hieu C. Nguyen <nc.hieu@live.com>
2020-04-20 16:28:26 +0700

    Create a wildcard feature (automatically recognize subdomain, and include wildcard)

--- src/certificate/loadCert.ts
+++ src/certificate/loadCert.ts
@@ -1,13 +1,18 @@
 import tls from "tls";
 import createCert from './createCert';
 import certificate from './certificate';
+import { getDomainName } from "./domainUtils";
 
-export async function loadCert(domain: string, email: string): Promise<tls.SecureContext> {
+export async function loadCert(servername: string, email: string): Promise<tls.SecureContext> {
+
+    let domain = getDomainName(servername);
 
     const exists = certificate.exists(domain, `key.pem`);
 
     if (!exists) {
-       await createCert({ domain, email });
+        let altNames;
+        servername === domain || servername.indexOf("_init-cert-wildcard") === 0 && (altNames = [`*.${domain}`]);
+       await createCert({ domain, email, altNames });
     }
 
     return tls.createSecureContext({
